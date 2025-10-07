1ddb89d52ba61ba03a378cf59faaf5769cfe6988
Deivissond894 <deivissons894@gmail.com>
2025-10-07 11:42:11 -0300

    Ajuste do ambiente firebase

--- services/firebaseService.js
+++ services/firebaseService.js
@@ -1,8 +1,22 @@
 // services/firebaseService.js
 const admin = require("firebase-admin");
-const serviceAccount = require("../cinesync-firebase.json"); // seu arquivo baixado do Firebase
 
 if (!admin.apps.length) {
+  // Usar variáveis de ambiente com os nomes exatos do seu Render
+  const serviceAccount = {
+    type: process.env.type,
+    project_id: process.env.PROJECT_ID,
+    private_key_id: process.env.PRIVATE_KEY_ID,
+    private_key: process.env.PRIVATE_KEY?.replace(/\\n/g, '\n'), // Corrige quebras de linha
+    client_email: process.env.client_email,
+    client_id: process.env.client_id,
+    auth_uri: process.env.auth_uri,
+    token_uri: process.env.token_uri,
+    auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+    client_x509_cert_url: process.env.client_x509_cert_url,
+    universe_domain: process.env.universe_domain
+  };
+
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
